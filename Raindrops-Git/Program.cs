db3d4c4e1cfeaf005fc27d32f1b093803dffa493
Unknown <prot@prot.com.pl>
2018-06-03 01:09:30 +0200

    Convert method completed
    Ready to go

--- Raindrops-Git/Program.cs
+++ Raindrops-Git/Program.cs
@@ -10,11 +10,39 @@ namespace Raindrops_Git
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Convert(30));
+            Console.ReadLine();
         }
 
         public static string Convert(int number)
-        {
-            throw new NotImplementedException("You need to implement this function.");
+        {            
+            var theFactors = Enumerable.Range(1, number).Where(n => number % n == 0).Where(m => m == 3 || m == 5 || m == 7);
+            if (theFactors.Count() > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var elem in theFactors)
+                    switch (elem)
+                    {
+                        case 3:
+                            sb.Append("Pling");
+                            break;
+                        case 5:
+                            sb.Append("Plang");
+                            break;
+                        case 7:
+                            sb.Append("Plong");
+                            break;
+                        default:
+                            break;
+                    }
+
+                return sb.ToString();
+            }
+            else
+            {
+                return System.Convert.ToString(number);
+            }
         } // end of Convert method
     }
 }
